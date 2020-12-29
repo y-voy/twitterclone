f1e1c3a95140f9bf3070be98115482663c2a038c
yuji moriya <voyager.nasa0928@gmail.com>
2020-12-29 21:29:29 +0900

    投稿の編集機能を追加

--- app/controllers/feelings_controller.rb
+++ app/controllers/feelings_controller.rb
@@ -1,4 +1,5 @@
 class FeelingsController < ApplicationController
+  before_action :set_feeling, only: [:edit, :update]
   def index
     @feelings = Feeling.all
   end
@@ -8,13 +9,25 @@ class FeelingsController < ApplicationController
   def create
     @feeling = Feeling.new(feeling_params)
     if @feeling.save
-      redirect_to new_feeling_path
+      redirect_to new_feeling_path, notice: '投稿しました！'
     else
       render :new
+    end
+  end
+  def edit
+  end
+  def update
+    if @feeling.update(feeling_params)
+      redirect_to feelings_path, notice: '編集しました！'
+    else
+      render :edit
     end
   end
   private
   def feeling_params
     params.require(:feeling).permit(:content)
+  end
+  def set_feeling
+    @feeling = Feeling.find(params[:id])
   end
 end
