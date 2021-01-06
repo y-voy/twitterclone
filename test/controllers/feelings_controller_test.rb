837cc428a261dc0900cf811c57d5d8def9090f27
yuji moriya <voyager.nasa0928@gmail.com>
2021-01-06 20:58:22 +0900

    詳細画面を追加し不要ファイルを整理

--- test/controllers/feelings_controller_test.rb
+++ test/controllers/feelings_controller_test.rb
@@ -1,7 +1,4 @@
 require 'test_helper'
 
 class FeelingsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
 end
--- test/models/feeling_test.rb
+++ test/models/feeling_test.rb
@@ -1,7 +1,4 @@
 require 'test_helper'
 
 class FeelingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 end
